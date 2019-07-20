09cee4f319fbb0d3f8d3743d8af43e4d845f9ff1
Morgan J Lopes <morgan@polarnotion.com>
2019-07-20 15:31:19 -0400

    Update seed data to include new user attributes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,18 @@
 puts "Creating Users."
 
+User.create(
+  first_name: "Morgan",
+  last_name: "Lopes",
+  email: "morgan@polarnotion.com",
+  password: '123123123',
+  is_admin: true
+)
+
 50.times do
-  user = User.create(email: Faker::Internet.email, password: '123123123')
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: '123123123'
+  )
 end
