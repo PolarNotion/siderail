54862b91188f77a3fbd59645cf6b0a0bad20774e
Morgan J Lopes <morgan@polarnotion.com>
2019-07-20 12:05:07 -0400

    Create nested admin controller with users#index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    resources :users, only: [ :index ]
+  end
+
   devise_for :users, controllers: {
     sessions:      'users/sessions',
     registrations: 'users/registrations',
