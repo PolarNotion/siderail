9dfe2730c5c478e32c9796eefecde260166b982c
Morgan J Lopes <morgan@polarnotion.com>
2019-07-20 15:16:32 -0400

    Setup active storage and add profile_photo to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :trackable
 
   has_paper_trail
+
+  has_one_attached :profile_photo
 end
