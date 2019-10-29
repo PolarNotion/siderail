abe6bea1f0acf50c922320191a482e2a348fa6b6
Loren Norman <lorennorman@gmail.com>
2019-10-29 12:00:55 -0400

    add simple terminal string colorization for better UX

--- application_template_helpers.rb
+++ application_template_helpers.rb
@@ -55,3 +55,23 @@ def assert_postgres
     Make sure you run `rails new` with the `-d postgresql` flag.
   POSTGRES
 end
+
+# Extend string with some Terminal color helpers
+class String
+  def red;        "\e[31m#{self}\e[0m" end
+  def green;      "\e[32m#{self}\e[0m" end
+  def yellow;     "\e[33m#{self}\e[0m" end
+  def blue;       "\e[34m#{self}\e[0m" end
+  def magenta;    "\e[35m#{self}\e[0m" end
+
+  def bg_red;     "\e[41m#{self}\e[0m" end
+  def bg_green;   "\e[42m#{self}\e[0m" end
+  def bg_blue;    "\e[44m#{self}\e[0m" end
+  def bg_magenta; "\e[45m#{self}\e[0m" end
+  def bg_cyan;    "\e[46m#{self}\e[0m" end
+  def bg_gray;    "\e[47m#{self}\e[0m" end
+
+  def bold;       "\e[1m#{self}\e[22m" end
+  def italic;     "\e[3m#{self}\e[23m" end
+  def underline;  "\e[4m#{self}\e[24m" end
+end
