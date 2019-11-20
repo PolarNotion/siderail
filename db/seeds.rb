fdf9dab9d82bb58e13f82b50194f263b0c9beaf5
Loren Norman <lorennorman@gmail.com>
2019-11-20 15:58:11 -0500

    simple seed for a dev user

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+puts "Creating Users."
+
+User.create(
+  first_name: "Bold",
+  last_name: "Developer",
+  email: "dev@polarnotion.com",
+  password: '123123123',
+  is_admin: true
+)
+
+50.times do
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: '123123123'
+  )
+end
